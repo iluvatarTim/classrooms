46b8f68b750265518c01113da79b85870f2867bc
Nathalie <nathalie.bouquet@epita.fr>
2018-03-22 14:43:32 +0100

    algopy

---
+++ S3/algopy/redblacktree.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+March 2018
+@author: nb
+"""
+
+class RedBlackTree:
+    def __init__(self, key, red=False, left=None, right=None):
+        """
+        Init Tree
+        """
+        self.key = key
+        self.left = left
+        self.right = right
+        self.red = red
+
+
+
+
