69d991acf3ed0d6be9b17afdf8963ae5e9ea62db
Nathalie <nathalie.bouquet@epita.fr>
2018-03-23 15:43:52 +0100

    new version
    - Dijsktra v. 2 (with list of "usefull" vertices) correction
    - TODO : Dijkstra with a heap adapted for ex. 1.2

--- S4/Dijkstra_S4.py
+++ S4/Dijkstra_S4.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 22 10:49:39 2017
+March 2018
 Graphs: Shortest Paths Dijkstra
 @author: Nathalie
 """
@@ -10,16 +10,22 @@ from algopy import graph
 
 inf = float('inf')
 
-G = graph.loadweightedgra("files/DijkstraTuto.wgra", int)
-
 # cost of edge x -> y
 # G.costs[(x, y)]
     
+
 def chooseMin(dist, M):
+    """
+    M: boolean vector, represents a set of vertices
+    returns the vertex in M that minimized the vector dist
+    if no vertex such that dist[x] != inf, returns None
+    """
+    x = None
+    mini = inf
     for i in range(len(dist)):
-        #FIXME
-        pass
-    
+        if M[i] and dist[i] < mini:
+            x = i
+            mini = dist[i]
     return x
     
 def Dijkstra(G, src):
@@ -29,7 +35,7 @@ def Dijkstra(G, src):
     M = [True] * G.order
     x = src
     n = 1
-    while dist[x] < inf and n < G.order:
+    while x != None and n < G.order    :
         M[x] = False
         for y in G.adjlists[x]:
             if dist[x] + G.costs[(x, y)] < dist[y]:
@@ -44,4 +50,57 @@ def Dijkstra(G, src):
 
 
 #----------------------------------------------------------------------------
+    
+"""
+    instead of working with a set of all vertices: 
+    here we have a list that contains "usefull" vertices
+"""
 
+def delMinInList(L, dist):
+    '''
+    returns and deletes the vertex in L (not empty) with minimum distance
+    '''
+    imin = 0
+    for i in range(1, len(L)):
+        if dist[L[i]] < dist[L[imin]]:
+            imin = i
+    x = L[imin]
+    L[imin] = L[-1]
+    L.pop()
+    return x
+
+def Dijkstra2(G, src, dst = None):
+    dist = [inf] * G.order
+    dist[src] = 0
+    p = [-1] * G.order    
+    L = [src]
+    while L != []:
+        x = delMinInList(L, dist)
+        for y in G.adjlists[x]:
+            if dist[x] + G.costs[(x, y)] < dist[y]:
+                if dist[y] == inf:
+                    L.append(y)
+                dist[y] = dist[x] + G.costs[(x, y)]
+                p[y] = x
+        
+    return (dist, p)
+    
+    
+#------------------------------------------------------------------------------
+# exercise 1.2: round trip
+# Optimization: Dijkstra with a heap 
+    
+from algopy import heap_spe as heap
+    
+def Dijkstra_withoutM(G, src, dst, M):
+    '''
+    src != dst
+    dst is reachable from src
+    the shortest path between src and dst without vertices in M
+    '''
+    #FIXME
+    pass
+
+def there_and_back(G, src, dst):
+    #FIXME
+    pass
