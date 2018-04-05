82a6bed95ab3f24bb23af79685268d6bf68a0b3c
Nathalie <nathalie.bouquet@epita.fr>
2018-04-05 17:48:59 +0200

    new version of login_projectManagement
    If you want a personnal correction you can upload your solution to
    https://sup-sub.cri.epita.net/projects/
    on "[s4] build my home" project
    before thursday 10:42 PM

--- S4/login_projectManagement.py
+++ S4/login_projectManagement.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-S4 - April 2018
+S4 - March 2017
 Shortest Path Homework
 @author: insert your login
 """
@@ -20,6 +20,44 @@ myHouse = [(2, []), (7, []), (3, [0, 1]), (1, [2]), (8, [0, 1]), (2, [3, 4]),
 #   G = graph.Graph(order, directed=True)     # a new digraph
 #   G.costs = {}                              # init costs (a dictionnary)
 #   G.costs[(x, y)] = val                     # add the cost of edge (x, y)
+
+def buildFromList(L):
+    """
+    L: for each task t, a pair (duration, list of tasks)
+    - duration: its duration (a float) !
+    - list of tasks: that have to be finished before t 
+    
+    returns:
+        G (Graph): the digraph with last two vertices added as vitual tasks 
+            task n is the beginning of the project
+            task n+1 is the end
+        G_1 (Graph): G reversed!
+        din, dout: vectors of in- and out- degrees in G
+    """
+    
+    #FIXME
+    
+    return (G, G_1, din, dout)
+
+def BellmanForProject(G, src, din):
+    """
+    return dist: the longuest paths from src to all vertices
+    all vertices are reachable from src
+    din: the in-degree vector
+    """
+    dist = [-inf] * G.order
+    dist[src] = 0
+    #FIXME
+    
+    while  #FIXME:
+        x = #FIXME
+        for y in G.adjlists[x]:
+            if dist[x] + G.costs[(x, y)] > dist[y]:
+                dist[y] = dist[x] + G.costs[(x, y)]
+            
+            #FIXME
+
+    return dist
 
 def project(L):
     """
